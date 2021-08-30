0df4cb12792ee9ffa6e1bef1f8ed0adf226da624
Valeriy Rotan <valeriy.rotan@berkeley.edu>
2021-08-29 20:01:50 -0700

    Working saveBookmark

--- src/background/background.ts
+++ src/background/background.ts
@@ -8,37 +8,40 @@ let smartMarkNode: BookmarkTreeNode
 let urlClassifier: UrlCategorizer
 
 import {UrlCategorizer} from "../libs/urlCategorizer"
+import {Bookmarks} from "../libs/bookmarks"
 
-function updateSmartMarkNode() {
-    chrome.bookmarks.getTree((tree: BookmarkTreeNode[]) => {
-        let folderPresent: boolean = false
-        tree[0].children![0].children!.forEach(element => {
-            if (element.title === 'SmartMark bookmarks') {
-                folderPresent = true;
-                smartMarkNode = element;
-                chrome.storage.sync.set({
-                    smartMarkNode: smartMarkNode,
-                });
-                console.log(smartMarkNode);
-            }
-        });
-        if (!folderPresent) {
-            console.log("Didn't find bookmark folder");
-            chrome.bookmarks.create({
-                    'parentId': "1",
-                    'title': 'SmartMark bookmarks'
-                },
-                function (newFolder) {
-                    smartMarkNode = newFolder;
-                    chrome.storage.sync.set({
-                        smartMarkNode: smartMarkNode,
-                    });
-                    console.log(smartMarkNode);
-                },
-            );
-        }
-    });
-}
+let bookmarks = new Bookmarks()
+
+// function updateSmartMarkNode() {
+//     chrome.bookmarks.getTree((tree: BookmarkTreeNode[]) => {
+//         let folderPresent: boolean = false
+//         tree[0].children![0].children!.forEach(element => {
+//             if (element.title === 'SmartMark bookmarks') {
+//                 folderPresent = true;
+//                 smartMarkNode = element;
+//                 chrome.storage.sync.set({
+//                     smartMarkNode: smartMarkNode,
+//                 });
+//                 console.log(smartMarkNode);
+//             }
+//         });
+//         if (!folderPresent) {
+//             console.log("Didn't find bookmark folder");
+//             chrome.bookmarks.create({
+//                     'parentId': "1",
+//                     'title': 'SmartMark bookmarks'
+//                 },
+//                 function (newFolder) {
+//                     smartMarkNode = newFolder;
+//                     chrome.storage.sync.set({
+//                         smartMarkNode: smartMarkNode,
+//                     });
+//                     console.log(smartMarkNode);
+//                 },
+//             );
+//         }
+//     });
+// }
 
 // *----*----*----* On-Install Script *----*----*----*
 chrome.runtime.onInstalled.addListener(() => {
@@ -49,27 +52,28 @@ chrome.runtime.onInstalled.addListener(() => {
             urlClassifier = new UrlCategorizer(urlMap);
         });
 
-    updateSmartMarkNode()
+    // updateSmartMarkNode()
 });
 
-function getBookmarks(sendResponse: { (a: { ok: boolean; bookmarks: BookmarkTreeNode[]; }): any }) {
-    chrome.bookmarks.getSubTree(smartMarkNode.id, (tree) =>
-        sendResponse({
-            ok: true,
-            bookmarks: tree
-        })
-    );
-}
+// function getBookmarks(sendResponse: { (a: object): any }) {
+//     chrome.bookmarks.getSubTree(smartMarkNode.id, (tree) =>
+//         sendResponse({
+//             ok: true,
+//             bookmarks: tree
+//         })
+//     );
+// }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.log(request.action)
     try {
         switch (request.action) {
-            case "get-bookmarks":
-                getBookmarks(sendResponse)
-                break
+            // case "get-bookmarks":
+            //     bookmarks.getBookmarks()
+            //     break
             case "save-bookmark":
-                saveBookmark(request.title, request.url).then(sendResponse)
+                let category = String(urlClassifier.getUrlCategory(request.url)).valueOf()
+                bookmarks.saveBookmark(request.url, request.title, [category]).then(sendResponse)
                 break
         }
     } catch (e) {
@@ -77,28 +81,27 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     }
 });
 
-async function saveBookmark(title: string, url: string) {
-    let folder = await createFolder(url);
-
-    chrome.bookmarks.create({
-            'parentId': folder.id,
-            'title': title,
-            'url': url
-        },
-        function (newBookmark) {
-            updateSmartMarkNode()
-            console.log("Saved bookmark: ", newBookmark);
-        },
-    );
-}
+// async function saveBookmark(title: string, url: string) {
+//     let folder = await createFolder(url);
+//
+//     chrome.bookmarks.create({
+//             'parentId': folder.id,
+//             'title': title,
+//             'url': url
+//         },
+//         function (newBookmark) {
+//             updateSmartMarkNode()
+//             console.log("Saved bookmark: ", newBookmark);
+//         },
+//     );
+// }
 
 function createFolder(url: string) {
 
     let category = String(urlClassifier.getUrlCategory(url)).valueOf()
 
     if (smartMarkNode.children) {
-        for (let child_id in smartMarkNode.children) {
-            let child = smartMarkNode.children[child_id]
+        for (const child of smartMarkNode.children) {
             if (child.title === category) {
                 return Promise.resolve(child)
             }
--- src/libs/bookmarks.ts
+++ src/libs/bookmarks.ts
@@ -1,103 +1,233 @@
 import BookmarkTreeNode = chrome.bookmarks.BookmarkTreeNode;
+import BookmarkChangeInfo = chrome.bookmarks.BookmarkChangeInfo;
 
 enum FindType {
     STRING_SEARCH
 }
 
-class Bookmarks {
+const BOOKMARK_FOLDER_TITLE = 'SmartMark'
 
-    /*
-    {
-        url: {
-            id: string
-            name: string
-            description: string
-            location: string
-            dateCreated: number (in ms)
-            lastModified: number (in ms)
-            lastAccessed: number (in ms)
-        }
-    }
+export class Bookmarks {
+    /**
+     * Container for all interactions with the bookmarks
+     * Completely encapsulates the chrome.bookmarks api
      */
-    private bookmarkMap = {}
 
-    // @ts-ignore
-    private rootNode: BookmarkTreeNode
+    /**
+     * node managing SmartMark folder
+     * {
+     *     url: {
+     *         chrome_id: string
+     *         url: string
+     *         name: string
+     *         description: string
+     *         location: string
+     *         dateCreated: number (in ms)
+     *         lastModified: number (in ms)
+     *         lastAccessed: number (in ms)
+     *     }
+     * }
+     */
+    private bookmarkMap: Record<string, object> = {}
 
-    // Updates the root node
-    private updateRoot() {
+    private rootNode!: BookmarkTreeNode
+
+    private findType: FindType
+
+    constructor(findType = FindType.STRING_SEARCH) {
+        this.findType = findType
+
+        this.updateSmartMarkNode()
+
     }
 
     /**********************/
     /* Bookmark-level API */
     /**********************/
 
-    // Save new or update existing bookmark name and location
-    saveBookmark(url: string, name: string, folder: string) {
+    // Save new or update existing bookmark title and location
+    async saveBookmark(url: string, title: string, folderName: string[]) {
+        let folder = await this.createFolder(folderName);
+
+        if (url in this.bookmarkMap) {
+            // @ts-ignore complains about the ['id']
+            const renamedBookmark = await chrome.bookmarks.update(this.bookmarkMap[url]['id'],
+                {
+                    'title': title,
+                },
+            );
+            // @ts-ignore
+            const finalBookmark = await chrome.bookmarks.move(renamedBookmark.id,
+                {
+                    'parentId': folder.id,
+                },
+            )
+            this.syncWithChrome()
+            console.log("Saved bookmark: ", finalBookmark);
+        } else {
+            chrome.bookmarks.create({
+                    'parentId': folder.id,
+                    'title': title,
+                    'url': url
+                },
+                (newBookmark) => {
+                    this.syncWithChrome()
+                    console.log("Saved bookmark: ", newBookmark);
+                },
+            );
+        }
     }
 
     // Update name for bookmark
-    renameBookmark(url: string, name: string) {
+    renameBookmark(url: string, title: string) {
+        // @ts-ignore
+        chrome.bookmarks.update(this.bookmarkMap[url]['id'],
+            {
+                'title': title,
+                'url': url
+            },
+            (newBookmark) => {
+                this.syncWithChrome()
+                console.log("Saved bookmark: ", newBookmark);
+            },
+        );
     }
 
     // Move bookmark to new folder
     moveBookmark(url: string, folder: string) {
+        // TODO
+        // chrome.bookmarks.move(this.bookmarkMap[url].id, )
     }
 
-    accessBookmark(url: string, date: number = -1) {
-    }
+    // TODO: Decide whether we need this is a good way to do this
+    // update the lastAccess date on the bookmark
+    // accessBookmark(url: string, date: number = -1) {
+    // }
 
     // Check if bookmark exists
     bookmarkExists(url: string) {
+        return url in this.bookmarkMap
     }
 
     // Get bookmark by id (url) or check if it exists (is null)
     getBookmark(url: string) {
+        return this.bookmarkMap[url]
     }
 
-    // Get bookmark by id (url) or check if it exists (is null)
-    getBookmarkAt(folder: string, name: string) {
+    // Delete bookmark by url
+    deleteBookmark(url: string) {
     }
 
     // Find best matching bookmarks by query string
     findBookmarks(query: string, method: FindType = FindType.STRING_SEARCH) {
     }
 
-    // Delete bookmark by id
-    deleteBookmark(url: string) {
-    }
-
-    // Delete bookmark by folder and name
-    deleteBookmarkAt(folder: string, name: string) {
-    }
-
     /********************/
     /* Folder-level API */
+
     /********************/
 
+    private createFolderShallow(root: BookmarkTreeNode, folder: string): Promise<BookmarkTreeNode> {
+        // if (!root.children) throw new SomeException()
+
+        for (const child of root.children!) {
+            if (child.title === folder)
+                return Promise.resolve(child)
+        }
+
+        return chrome.bookmarks.create({
+            'parentId': root.id,
+            'title': folder,
+        })
+    }
+
     // Ensure the folder exists
-    createFolder(folder: string) {
+    async createFolder(folder: string[]) {
+
+        let root = this.rootNode
+        console.log("root", root)
+        for (const folderLevel of folder) {
+            root = await this.createFolderShallow(root, folderLevel)
+        }
+        return root
     }
 
     // Check if bookmark exists
-    folderExists(folder: string): boolean {
+    folderExists(folder: string[]): boolean {
         return false;
     }
 
     // Get all bookmarks in folder
-    getBookmarks(folder: string) {
+    getBookmarks(folder: string[]) {
     }
 
     // Get all subfolders in folder
-    getSubfolders(folder: string) {
+    getSubfolders(folder: string[]) {
     }
 
-    // Find best matching bookmarks by query string
+    // Find best matching folders by query string
     findFolders(query: string, method: FindType = FindType.STRING_SEARCH) {
     }
 
     // Delete a whole folder, or a bookmark by name
-    deleteFolder(folder: string) {
+    deleteFolder(folder: string[]) {
+    }
+
+    /***********/
+    /* Helpers */
+
+    /***********/
+
+    updateSmartMarkNode() {
+        chrome.bookmarks.getTree(tree => {
+            let folderPresent: boolean = false
+
+            tree[0].children![0].children!.forEach(element => {
+                if (element.title === BOOKMARK_FOLDER_TITLE) {
+                    folderPresent = true;
+                    this.rootNode = element;
+                }
+            });
+            if (!folderPresent) {
+                console.log("Didn't find bookmark folder. Creating one now...");
+                chrome.bookmarks.create({
+                        'parentId': "1",
+                        'title': BOOKMARK_FOLDER_TITLE
+                    },
+                    newFolder => this.rootNode = newFolder
+                )
+            }
+        })
+    }
+
+    visitSubtree(tree: BookmarkTreeNode, leafCallback: (a: BookmarkTreeNode) => any) {
+        if (tree.children)
+            tree.children.forEach(subtree => this.visitSubtree(subtree, leafCallback))
+        else
+            leafCallback(tree)
+    }
+
+    updateHashMap() {
+        this.bookmarkMap = {}
+        this.visitSubtree(this.rootNode, (leaf: BookmarkTreeNode) => {
+            this.bookmarkMap[leaf.url!] = leaf // TODO change to custom object
+        })
+    }
+
+    /**
+     * Syncs local bookmark data structres with the chrome bookmarks api
+     * @private
+     */
+    private syncWithChrome() {
+        this.updateSmartMarkNode()
+        this.updateHashMap()
     }
 
 }
+
+// TODO
+chrome.bookmarks.onChanged.addListener((id: string, changeInfo: BookmarkChangeInfo) => {
+    console.log('changeInfo')
+    console.log(id)
+    console.log(changeInfo)
+})
