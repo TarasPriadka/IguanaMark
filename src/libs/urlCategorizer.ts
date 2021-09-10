f802a6d93a3fe348b9f51a6d8140d24f48a29c73
Taras Priadka <taraspriadka@berkeley.edu>
2021-09-10 12:25:29 -0700

    Document UrlCategorizer. Return all of the urls with the smallest distances.

--- src/libs/urlCategorizer.ts
+++ src/libs/urlCategorizer.ts
@@ -7,6 +7,12 @@ export class UrlCategorizer {
     urlMap: Record<string, string> | undefined
     levensteinThreshold: number
 
+    /**
+     * Creates a UrlCategorizer
+     *
+     * @param urlMap maps url host to the category
+     * @param levThreshold maximum url Levenshtein distance
+     */
     constructor(urlMap?: Record<string, string>, levThreshold = 5) {
         if (urlMap != undefined) {
             this.urlMap = urlMap;
@@ -14,6 +20,12 @@ export class UrlCategorizer {
         this.levensteinThreshold = levThreshold;
     }
 
+    /**
+     * Use Url map to get the category of the current url
+     *
+     * @param url link to categorize
+     * @returns string category of the URL
+     */
     getUrlCategory(url: string) {
         if (this.urlMap != undefined) {
             let u = new URL(url);
@@ -23,16 +35,32 @@ export class UrlCategorizer {
         }
     }
 
+    /**
+     * Use Levenshtein edit distance to find Url which is the closest to the `url` out of list of other urls.
+     *
+     * @param url base url
+     * @param otherUrls list of all urls
+     * @returns list of urls which are the closest to the currently bookmarked
+     */
     getMostSimilarUrl(url: string, otherUrls: string[]) {
         let minDistance = BIG_INTEGER;
-        let minUrl = "";
+        let minUrls: [string, number][] = [] //array which will collect all of the urls with scores in ascending order
         for (const urlElement of otherUrls) {
-            let dist = levenshtein.get(url, urlElement);
-            if (dist <= this.levensteinThreshold && dist < minDistance) {
+            let dist: number = levenshtein.get(url, urlElement);
+            if (dist <= this.levensteinThreshold && dist <= minDistance) {
                 minDistance = dist;
-                minUrl = urlElement;
+                minUrls.unshift([urlElement, dist]);
             }
         }
-        return minUrl;
+        let outputUrls: string[] = [];
+
+        if (minUrls.length > 0) {
+            let min = minUrls[0][1];
+            let i=0;
+            for (; i < minUrls.length && minUrls[i][1] == min; i++); //find index of elem w/ distance > min
+            outputUrls = minUrls.map((url) => url[0]).slice(0,i);
+        }
+
+        return outputUrls;
     }
 }
