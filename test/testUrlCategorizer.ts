781259632486fb892e6f07fcc1114afca067dfae
Taras Priadka <taraspriadka@berkeley.edu>
2021-09-10 12:25:29 -0700

    Document UrlCategorizer. Return all of the urls with the smallest distances.

--- test/testUrlCategorizer.ts
+++ test/testUrlCategorizer.ts
@@ -8,12 +8,12 @@ describe('URL Categorizer Tests', () => {
         let inputURL = "www.project.com/1";
         let inputOtherURLs = ["www.project.com/2", "www.youtube.com/view?", "www.google.com"]
 
-        console.debug("Input URL", inputURL);
-        console.debug("Other URLs", inputOtherURLs);
+        // console.debug("Input URL", inputURL);
+        // console.debug("Other URLs", inputOtherURLs);
 
         let output = categorizer.getMostSimilarUrl(inputURL, inputOtherURLs);
-        console.debug("Output: ", output);
-        assert.equal(output, "www.project.com/2");
+        // console.debug("Output: ", output);
+        assert.deepEqual(output, ["www.project.com/2"], "Arrays are not equal");
     });
 
     it('Checking no similar URLs', () => { // the single test
@@ -22,11 +22,25 @@ describe('URL Categorizer Tests', () => {
         let inputURL = "www.project.com/1";
         let inputOtherURLs = ["www.instagram.com/helloworld", "www.facebook.com/view?user", "www.google.com"]
 
-        console.debug("Input URL", inputURL);
-        console.debug("Other URLs", inputOtherURLs);
+        // console.debug("Input URL", inputURL);
+        // console.debug("Other URLs", inputOtherURLs);
         let output = categorizer.getMostSimilarUrl(inputURL, inputOtherURLs);
-        console.debug("Output: ", output);
+        // console.debug("Output: ", output);
 
-        assert.equal(output, "");
+        assert.deepEqual(output, [], "Arrays are not equal");
+    });
+
+    it('Checking multiple similar URLs', () => { // the single test
+        let categorizer = new UrlCategorizer(); // this will be your class
+
+        let inputURL = "www.project.com/1";
+        let inputOtherURLs = ["www.project.com/2", "www.project.com/3", "www.project.com/4", "www.youtube.com/view?", "www.google.com"]
+
+        // console.debug("Input URL", inputURL);
+        // console.debug("Other URLs", inputOtherURLs);
+        let output = categorizer.getMostSimilarUrl(inputURL, inputOtherURLs);
+        // console.debug("Output: ", output);
+
+        assert.deepEqual(output, ["www.project.com/4", "www.project.com/3", "www.project.com/2"], "Arrays are not equal");
     });
 });
