ab46284a04a6c63914ed7a84565f16b704f89a18
Taras Priadka <taraspriadka@berkeley.edu>
2021-09-10 11:29:25 -0700

    URL similarity code

--- src/libs/urlCategorizer.ts
+++ src/libs/urlCategorizer.ts
@@ -1,9 +1,17 @@
+let levenshtein = require('fast-levenshtein');
+
+const BIG_INTEGER = 100000
+
 export class UrlCategorizer {
 
     urlMap: Record<string, string> | undefined
+    levensteinThreshold: number
 
-    constructor(urlMap?: Record<string, string>) {
-        this.urlMap = urlMap;
+    constructor(urlMap?: Record<string, string>, levThreshold = 5) {
+        if (urlMap != undefined) {
+            this.urlMap = urlMap;
+        }
+        this.levensteinThreshold = levThreshold;
     }
 
     getUrlCategory(url: string) {
@@ -16,9 +24,15 @@ export class UrlCategorizer {
     }
 
     getMostSimilarUrl(url: string, otherUrls: string[]) {
-        // for (let urlKey in otherUrls) {
-        //
-        // }
-        return "";
+        let minDistance = BIG_INTEGER;
+        let minUrl = "";
+        for (const urlElement of otherUrls) {
+            let dist = levenshtein.get(url, urlElement);
+            if (dist <= this.levensteinThreshold && dist < minDistance) {
+                minDistance = dist;
+                minUrl = urlElement;
+            }
+        }
+        return minUrl;
     }
 }
