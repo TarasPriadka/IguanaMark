77d0f7cd172eddf6da1373d3ff62f9f54c13bebf
Taras Priadka <taraspriadka@berkeley.edu>
2021-09-11 15:41:41 -0700

    Remove debugging logs

--- test/testUrlCategorizer.ts
+++ test/testUrlCategorizer.ts
@@ -8,24 +8,17 @@ describe('URL Categorizer Tests', () => {
         let inputURL = "www.project.com/1";
         let inputOtherURLs = ["www.project.com/2", "www.youtube.com/view?", "www.google.com"]
 
-        // console.debug("Input URL", inputURL);
-        // console.debug("Other URLs", inputOtherURLs);
-
         let output = categorizer.getMostSimilarUrl(inputURL, inputOtherURLs);
-        // console.debug("Output: ", output);
         assert.deepEqual(output, ["www.project.com/2"], "Arrays are not equal");
     });
 
-    it('Checking no similar URLs', () => { // the single test
-        let categorizer = new UrlCategorizer(); // this will be your class
+    it('Checking no similar URLs', () => { 
+        let categorizer = new UrlCategorizer(); 
 
         let inputURL = "www.project.com/1";
         let inputOtherURLs = ["www.instagram.com/helloworld", "www.facebook.com/view?user", "www.google.com"]
 
-        // console.debug("Input URL", inputURL);
-        // console.debug("Other URLs", inputOtherURLs);
         let output = categorizer.getMostSimilarUrl(inputURL, inputOtherURLs);
-        // console.debug("Output: ", output);
 
         assert.deepEqual(output, [], "Arrays are not equal");
     });
@@ -36,10 +29,7 @@ describe('URL Categorizer Tests', () => {
         let inputURL = "www.project.com/1";
         let inputOtherURLs = ["www.project.com/2", "www.project.com/3", "www.project.com/4", "www.youtube.com/view?", "www.google.com"]
 
-        // console.debug("Input URL", inputURL);
-        // console.debug("Other URLs", inputOtherURLs);
         let output = categorizer.getMostSimilarUrl(inputURL, inputOtherURLs);
-        // console.debug("Output: ", output);
 
         assert.deepEqual(output, ["www.project.com/4", "www.project.com/3", "www.project.com/2"], "Arrays are not equal");
     });
