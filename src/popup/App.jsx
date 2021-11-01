5e69989fe47f3da595750ad948b4f266b4970b43
Taras Priadka <taraspriadka@berkeley.edu>
2021-10-31 22:24:21 -0700

    Started quick mark button integration.

--- src/popup/App.jsx
+++ src/popup/App.jsx
@@ -4,22 +4,33 @@ import "./App.css";
 
 import AppNavbar from "./components/AppNavbar.jsx";
 import ListItemContainer from "./components/ListItemContainer.jsx";
-import {iguanaClickedAtom} from "./atoms.js";
-import {useRecoilValue} from "recoil";
+import {iguanaClickedAtom, quickMarkVisibleAtom} from "./atoms.js";
+
+import {useRecoilState, useRecoilValue} from "recoil";
 import ListItemForm from "./components/ListItemCreationForm.jsx";
 
 
 function App() {
     const iguanaClicked = useRecoilValue(iguanaClickedAtom);
+    let [quickMarkVisible, setQuickMarkVisible] = useRecoilState(quickMarkVisibleAtom);
 
-    return <div className="container-fluid App p-1">
-        <AppNavbar/>
-        {iguanaClicked ? <ListItemForm/> : <></>}
-        <hr/>
-        <div className="scrollable">
-            <ListItemContainer/>
+    return <>
+        <div className="container-fluid App p-1">
+            <AppNavbar/>
+            {iguanaClicked ? <ListItemForm/> : <></>}
+            <hr/>
+            <div className="scrollable">
+                <ListItemContainer/>
+            </div>
         </div>
-    </div>
+        <div className={"checkbox-wrapper noselect pl-3"} onClick={() => {
+            setQuickMarkVisible(!quickMarkVisible);
+        }}>
+                    <input type="checkbox" checked={quickMarkVisible}/>
+                    <span className="animated"/>
+                    Quick Mark Button
+                </div>
+    </>
 }
 
 export default App;
