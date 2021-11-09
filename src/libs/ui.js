1acdb18022a71889f1238da97b4d7b443220a587
Taras Priadka <taraspriadka@berkeley.edu>
2021-11-08 21:32:10 -0800

    Rename ui and recreate package-lock.

---
+++ src/libs/ui.js
@@ -0,0 +1,56 @@
+/**
+ * Wrapper for chrome.runtime.sendMessage with a try-catch block.
+ *
+ * @param message message to send
+ * @param callback callback for the message. Optional.
+ */
+function sendMessage(message, callback) {
+    try {
+        return chrome.runtime.sendMessage(message, callback);
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+/**
+ * Saves bookmark
+ *
+ * @param title title of page.
+ * @param url URL of the page to save.
+ * @param tab tabId from which the request to bookmark is sent. Optional.
+ */
+export async function saveCurrentPage(title, url, tab) {
+    return sendMessage({
+        action: "save-bookmark",
+        title: title,
+        url: url,
+        tab: tab
+    })
+}
+
+/**
+ * Removes bookmark with given URL
+ * @param url URL of bookmark to remove
+ */
+export async function removeCurrentPage(url) {
+    return sendMessage({
+            action: "remove-bookmark",
+            url: url,
+        },
+    );
+}
+
+/**
+ * Checks whether bookmark exists.
+ *
+ * @param url URL of bookmark to check
+ * @param callback callback with the boolean exists or not.
+ */
+export function getBookmarkExists(url, callback) {
+    return sendMessage({
+            action: "check-bookmark",
+            url: url,
+        },
+        callback
+    );
+}
