81042a9a8703aa0a1e8e540a352b8211f397153a
Taras Priadka <taraspriadka@berkeley.edu>
2021-11-09 20:21:25 -0800

    Implement searching when search text is not empty.

--- src/popup/components/ListItemContainer.jsx
+++ src/popup/components/ListItemContainer.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import "../App.css";
 import ListItem from "./listItem/ListItem";
 import {useRecoilValue} from "recoil";
-import {listItemsAtom} from "../atoms";
+import {listItemsAtom, searchTextAtom} from "../atoms";
 
 
 function ListItemContainer() {
 
     const listItems = useRecoilValue(listItemsAtom);
+    const searchText = useRecoilValue(searchTextAtom);
 
-    console.log(listItems)
-
-    const displayBoxes = listItems.map((linkObj, index) => {
+    const displayBoxes = listItems.filter((linkObj) => {
+        if (searchText === "") {
+            return true;
+        } else {
+            console.log(linkObj);
+            let searchLower = searchText.toLowerCase();
+            return linkObj.title.toLowerCase().includes(searchLower) || linkObj.tags.some(tag => tag.toLowerCase().includes(searchLower));
+        }
+    }).map((linkObj, index) => {
         return <ListItem
             key={linkObj["url"]}
             className="m-2"
